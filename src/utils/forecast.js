32822fa2918b81f73d006f81431d71275a2a3177
Nandita Prasad <nandita97pd@gmail.com>
2020-12-30 02:44:12 +0530

    Add Humidity Parameter

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find the location!!',undefined)
         }
         else{
-            callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out. It feels like "+body.current.feelslike+" degrees out.")
+            callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out. It feels like "+body.current.feelslike+" degrees out. The humidity is "+body.current.humidity+"%.")
         }
     })
 }
